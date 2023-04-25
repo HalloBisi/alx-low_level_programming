6125c777496af1923c3a11ae7876a00c1b9d7b1b
HalloBisi <bisi2g9@gmail.com>
2023-04-24 20:13:34 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description : 'Print all possible combinations of two two-digit numbers'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int o;
+
+	for (n = 48; n <= 55; n++)
+	{
+		for ( m = n + 1; m <= 56; m++)
+		{
+			for (o = m + 1; o <= 57; o++)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(o);
+				if (n == 55 && m == 56 && o == 57)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
