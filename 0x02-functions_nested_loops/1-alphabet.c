92a5703665252a9a1762f6faacda033630492d4d
HalloBisi <bisi2g9@gmail.com>
2023-04-26 05:38:07 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Description: 'Print alphabets using _purchar'
+ * Retun: Always 0 (success)
+ */
+void print(void)
+{
+	int n;
+
+	    for (n = 97; n <= 122; n++)
+	    {
+		    _putchar(n);
+	   }
+	_putchar('\n');
+}
+
