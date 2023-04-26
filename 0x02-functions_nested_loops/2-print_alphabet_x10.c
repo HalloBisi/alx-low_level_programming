0356fdb09bb1dcb6369585e7c2b33a87a6a32fe6
HalloBisi <bisi2g9@gmail.com>
2023-04-26 06:38:35 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print 10 times the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 97; m <= 122; m++)
+		{
+			_putchar(n);
+		}
+	_putchar('\n');
+	}
+}
