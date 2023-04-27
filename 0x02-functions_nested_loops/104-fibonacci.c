babd9715793056ee38b4a76e5fee0d86bd2ffac3
HalloBisi <bisi2g9@gmail.com>
2023-04-27 07:32:41 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int main(void)
 	for (a = 92; a < 99; ++a)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % 1);
+		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
