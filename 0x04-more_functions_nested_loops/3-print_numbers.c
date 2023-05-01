c8c092dcc4ef32d06aee8009015beb23c3e3a894
HalloBisi <bisi2g9@gmail.com>
2023-05-01 04:21:38 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,8 @@ void print_numbers(void)
 	int a;
 	{
 
-		for (a = 48; a <= 57; i++)
-			_putchar(i);
+		for (a = 48; a <= 57; a++)
+			_putchar(a);
 	}
-		_putchar('\n');
-		return (0);
+	_putchar('\n');
 }
