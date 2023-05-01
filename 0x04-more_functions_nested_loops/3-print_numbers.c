fbd8ddabfcf661d07401cdc7b59859fd695af2b4
HalloBisi <bisi2g9@gmail.com>
2023-05-01 04:12:02 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - Print numbers 0 -9 followed by new line
+*
+*Return: Always 0 for success
+*/
+void print_numbers(void)
+{
+	int a;
+	{
+
+		for (a = 48; a <= 57; i++)
+			_putchar(i);
+	}
+		_putchar('\n');
+		return (0);
+}
