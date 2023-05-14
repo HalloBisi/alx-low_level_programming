eb0108097d6df6b81e24ca81dbe0d1d5b4f0726e
HalloBisi <bisi2g9@gmail.com>
2023-05-14 12:24:46 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,18 +2,14 @@
 #include <stdio.h>
 /**
 *main -Entry point
-*@argc: inout value
-*@argv: input value
+*@argc: number of arguments
+*@argv: array of arguments
 *Return: Always 0 success
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
+	printf("%s ", *argv);
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
 	printf("\n");
 	return (0);
 }
