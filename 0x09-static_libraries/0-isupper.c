2bc1507bab9999121fca0ee74af82e23f9c446bd
HalloBisi <bisi2g9@gmail.com>
2023-05-17 13:15:34 -0700

    Static Lib

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isupper - Print Upper case
+*
+*@c: Upper case
+*
+*Return: 0 or 1
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
